bcee5bd8951b1598c64273c9bbb2cc7aa5d62233
kaytran1419 <dangkhoa4244@gmail.com>
2025-10-17 23:03:23 +0700

    chuẩn bị deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,18 +3,30 @@ import taskRoute from "./router/taskRouter.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 8080;
+const __dirname = path.resolve();
 
 const app = express();
 
 //middlewares
-app.use(cors({ origin: "http://localhost:5173" }));
+if (process.env.NODE_ENV !== "production") {
+  app.use(cors({ origin: "http://localhost:5173" }));
+}
 app.use(express.json());
 
 app.use("/api/task", taskRoute);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
